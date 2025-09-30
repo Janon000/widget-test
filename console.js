f9e0695bc48449cf2e40ffefba0bd23b8bcf75ba
Brictoni Piper <81114883+Janon000@users.noreply.github.com>
2025-09-30 18:41:07 -0500

    Update console.js

--- console.js
+++ console.js
@@ -1,1 +1,14 @@
-console.log("Does this work?")
+// Create a global object to hold your widget's logic
+const myWidget = {
+    // The init function will be called by the bridge script
+    init: function(fieldData) {
+        console.log("External script initialized!");
+        console.log("Received fieldData:", fieldData);
+
+        // All of your widget's logic now goes here.
+        // You have access to the settings via the 'fieldData' parameter.
+        
+        applyAppearanceStyles(fieldData);
+        // ... and any other functions you have.
+    }
+};
